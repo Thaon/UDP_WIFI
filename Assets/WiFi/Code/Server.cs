0be22abc7e3163f77a32850405851944fcc800a1
thaon <commerce@gabe.software>
2021-08-03 19:06:30 +0200

    Completed comments

--- Assets/WiFi/Code/Server.cs
+++ Assets/WiFi/Code/Server.cs
@@ -11,6 +11,9 @@ namespace LocalNetworking
 {
     public class Server : MonoBehaviour
     {
+        /// <summary>
+        /// The size of the buffer used to send and receive data
+        /// </summary>
         private const int BUFFER_SIZE = 100 * 1024;
 
         #region nested classes
@@ -29,10 +32,23 @@ namespace LocalNetworking
 
         #endregion
 
+        /// <summary>
+        /// should we debug the communications?
+        /// </summary>
+        public bool _debug = false;
+
+        /// <summary>
+        /// is this instance a host?
+        /// </summary>
         [HideInInspector]
         public bool host;
+        /// <summary>
+        /// the port used for communications, MUST BE ABOVE 3K
+        /// </summary>
         public int _port = 8008;
-        public bool _debug = false;
+        /// <summary>
+        /// the socket we will use for communications
+        /// </summary>
         public Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         /// <summary>
@@ -43,27 +59,34 @@ namespace LocalNetworking
         public Action OnServerShutdown;
 
         /// <summary>
-        /// Networking handlers
+        /// Using a thread so we don't block the program's execution while listening
         /// </summary>
         private Thread _clientThread;
+        /// <summary>
+        /// list of the connected clients
+        /// </summary>
         private List<Socket> _clients = new List<Socket>();
+        /// <summary>
+        /// buffer used for sending/receiving data
+        /// </summary>
         private byte[] _buffer = new byte[BUFFER_SIZE];
 
 
-        private void OnDisable()
-        {
-            if (host) CloseAllSockets(); else CloseClientConnection();
-        }
-
+        /// <summary>
+        /// We initiate a server session
+        /// </summary>
         public void Host()
         {
             if (_debug) Debug.LogError("Started Host");
             host = true;
             _socket.Bind(new IPEndPoint(IPAddress.Any, _port));
             _socket.Listen(100);
-            _socket.BeginAccept(Listen, null);
+            _socket.BeginAccept(ListenForConnections, null);
         }
 
+        /// <summary>
+        /// we initiate a client session
+        /// </summary>
         public void Join()
         {
             if (_debug) Debug.LogError("Started Client");
@@ -84,12 +107,38 @@ namespace LocalNetworking
                 }
             }
 
-            _clientThread = new Thread(new ThreadStart(ClientListen));
+            _clientThread = new Thread(new ThreadStart(ListenForData));
             _clientThread.IsBackground = true;
             _clientThread.Start();
         }
 
-        private void ClientListen()
+        /// <summary>
+        /// thread used to listen for incoming connections
+        /// </summary>
+        private void ListenForConnections(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = _socket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+
+            _clients.Add(socket);
+            OnConnection?.Invoke(socket);
+            socket.BeginReceive(_buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveData, socket);
+            _socket.BeginAccept(ListenForConnections, null);
+
+        }
+
+        /// <summary>
+        /// thread used to listen for incoming data
+        /// </summary>
+        private void ListenForData()
         {
             while (true)
             {
@@ -107,17 +156,15 @@ namespace LocalNetworking
             }
         }
 
+        /// <summary>
+        /// fire up events when data is received, this happens on the main thread
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
         private IEnumerator OnDataCO(Message msg)
         {
             OnData?.Invoke(msg);
             yield return new WaitForEndOfFrame();
-        }
-
-        private void CloseClientConnection()
-        {
-            Send("CLIENT_EXIT", ""); // Tell the server we are exiting
-            _socket.Shutdown(SocketShutdown.Both);
-            _socket.Close();
         }
 
         public void Send(string opCode, string text)
@@ -141,26 +188,10 @@ namespace LocalNetworking
             }
         }
 
-        private void Listen(IAsyncResult AR)
-        {
-            Socket socket;
-
-            try
-            {
-                socket = _socket.EndAccept(AR);
-            }
-            catch (ObjectDisposedException)
-            {
-                return;
-            }
-
-            _clients.Add(socket);
-            OnConnection?.Invoke(socket);
-            socket.BeginReceive(_buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveData, socket);
-            _socket.BeginAccept(Listen, null);
-
-        }
-
+        /// <summary>
+        /// both the server and the client will use this to process received data
+        /// </summary>
+        /// <param name="AR"></param>
         private void ReceiveData(IAsyncResult AR)
         {
             Socket current = (Socket)AR.AsyncState;
@@ -172,17 +203,19 @@ namespace LocalNetworking
             }
             catch (SocketException)
             {
-                Debug.LogWarning("Client forcefully disconnected");
-                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                //close the connection and clean up if there's an issue
+                if (_debug) Debug.LogWarning("Client disconnected");
                 current.Close();
                 _clients.Remove(current);
                 return;
             }
 
+            //get the data
             byte[] recBuf = new byte[received];
             Array.Copy(_buffer, recBuf, received);
-            string messageJson = Encoding.ASCII.GetString(recBuf);
 
+            //decode the message
+            string messageJson = Encoding.ASCII.GetString(recBuf);
             Message msg = JsonUtility.FromJson<Message>(UnpackJson(messageJson));
 
             if (_debug) Debug.LogError(msg._opCode + " - " + msg._msg);
@@ -207,15 +240,29 @@ namespace LocalNetworking
                     return;
             }
 
-            //broadcast message back
+            //broadcast message back on the server
             if (host) _clients.ForEach(socket =>
             {
                 socket.Send(recBuf);
             });
 
+            //restart the cycle
             current.BeginReceive(_buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveData, current);
         }
 
+        /// <summary>
+        /// clean exit for the client
+        /// </summary>
+        private void CloseClientConnection()
+        {
+            Send("CLIENT_EXIT", ""); // Tell the server we are exiting
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        /// <summary>
+        /// complete server shutdown
+        /// </summary>
         private void CloseAllSockets()
         {
             //notify clients of the imminent server shutdown
@@ -235,7 +282,13 @@ namespace LocalNetworking
                 socket.Close();
             }
 
+            //close the socket
             _socket.Close();
+        }
+
+        private void OnDisable()
+        {
+            if (host) CloseAllSockets(); else CloseClientConnection();
         }
 
         #region utility
@@ -257,7 +310,6 @@ namespace LocalNetworking
         public void SendJSON<T>(string opCode, T json)
         {
             string toSend = JsonUtility.ToJson(json);
-
             Send(opCode, toSend);
         }
 
